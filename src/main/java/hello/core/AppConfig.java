aabeb8545d5f42b29fa1d8836c2bbb7d99cea0a1
YOUNGHWA KIM <haileykim2014@gmail.com>
2021-09-21 02:16:57 +0900

    스프링 빈 조회 - 기본

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.security.PublicKey;
 
+@Configuration
 public class AppConfig {
     //AppConfig는 생성한 객체 인스턴스의 참조를 생성자를 통해서 주입해준다.
     //역할과 구현클래스가 한눈에 들어오게 바꾸기
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());// 생성자를 통해 객체가 들어간다. ->생성자주입 DI 의존관계주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
     //    return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();//appConfig에게 멤버서비스를 달라고부탁->memberServiceImpl을 리턴함
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();//appConfig에게 멤버서비스를 달라고부탁->memberServiceImpl을 리턴함
         //MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
