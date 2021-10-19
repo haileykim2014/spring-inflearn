2dad7244d32e4a7ce25885b86d5f0f9b896cf5d0
YOUNGHWA KIM <haileykim2014@gmail.com>
2021-10-20 01:05:17 +0900

    애노테이션 직접만들기
    애노테이션에는 상속이라는 개념이 없다. 이렇게 여러 애노테이션을 모아서 사용하는 기능은 스프링이
    지원해주는 기능이다. @Qulifier 뿐만 아니라 다른 애노테이션들도 함께 조합해서 사용할 수 있다.
    단적으로 @Autowired도 재정의 할 수 있다. 물론 스프링이 제공하는 기능을 뚜렷한 목적 없이 무분별하게
    재정의 하는 것은 유지보수에 더 혼란만 가중할 수 있다

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -11,14 +12,13 @@ import org.springframework.stereotype.Component;
 //2. 연간관계 자동주입
 
 @Component
-//@RequiredArgsConstructor//final붙은 필드의 파라미터생성자를 만들어준다
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;//관심사의 분리 : 객체를 생성하고 연결하는 역할과 실행하는 역할이 명확히 분리되었다.
     private final DiscountPolicy discountPolicy;//생성자에 의해 값이 할당된다.인터페이스에만 의존하고 구체적인 클래스에 의존하지않는다, 어떤repository가 넣어질지 모르는상태
     //final를 넣으면 생성자에서 값을 넣어주도록 한다.
 
-    @Autowired//같은타입에 여러개가있다면 파라미터이름에 맞게 가져온다. //@Qualifier끼리 매칭 , 빈 이름 매칭,
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+    @Autowired//같은타입에 여러개가있다면 파라미터이름에 맞게 가져온다. //생성자 자동주입
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
