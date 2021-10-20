ba235f7c2047bded9139d4fc816ee6b3e82980e1
YOUNGHWA KIM <haileykim2014@gmail.com>
2021-10-21 00:04:40 +0900

    @PostConstruct, @PreDestroy 애노테이션 특징

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,6 +18,8 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig{
+        //유일한 단점은 외부 라이브러리에는 적용하지 못한다는 것이다. 외부 라이브러리를 초기화, 종료 해야 하면
+        //@Bean의 기능을 사용하자
         @Bean
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
